90b7263b988fe31b6017ced8cec5a44dfd8d9185
Yuriy Stos <100sbsh@gmail.com>
2016-07-27 13:10:57 +0300

    Enabled pretty urls

--- config/web.php
+++ config/web.php
@@ -38,14 +38,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
